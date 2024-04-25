3473c5d45d88d3b46fcc6879f25cfac6cb77f10f
bedialp <fvzlpktk@gmail.com>
2024-04-25 22:24:20 +0300

    Add project files.

---
+++ OneMusicEntityLayer/Entities/Singer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMusic.EntityLayer.Entities
+{
+    public class Singer
+    {
+        public int SingerId { get; set; }
+        public String Name { get; set; }
+        public String ImageUrl { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
